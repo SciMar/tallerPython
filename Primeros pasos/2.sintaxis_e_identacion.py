d802e72e3feeaea769fbd3f2a78fca01b4937524
SciMar <cramirezanzola@gmail.com>
2026-07-01 19:48:17 -0500

    Lógica de programación y control de flujo

---
+++ Primeros pasos/2.sintaxis_e_identacion.py
@@ -0,0 +1,5 @@
+if 5 > 3:
+    if 4 >3:
+        print("4 is greater than 3")
+    
+    print("5 is greater than 3")
---
+++ Primeros pasos/3.comentarios.py
@@ -0,0 +1,13 @@
+# Esta es una línea de comentario
+print("Hola mundo") # Instrucción de código
+
+
+
+"""
+lkdjdkfdkfjdkfjdkfjdf
+dfdfdfd
+dfdf
+ddfdf
+dfdf
+
+"""
